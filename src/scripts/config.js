1563160e0ac14e5dd0f8f2603e17046d3f6e4c22
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-05-29 11:39:43 +0200

    first part

---
+++ src/scripts/config.js
@@ -0,0 +1,10 @@
+export const API_PATH = 'https://pixabay.com/api';
+export const API_KEY = '36836755-9f43607b903fa703cdff42e50';
+
+export const DEFAULT_PIXABAY_PARAMS = {
+  key: API_KEY,
+  per_page: '20',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+};
