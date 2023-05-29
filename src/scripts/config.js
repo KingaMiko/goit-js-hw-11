2e63ead4487c1d139d52ce05ff50ae9dce0562d1
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-05-29 11:59:02 +0200

    api key

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 export const API_PATH = 'https://pixabay.com/api';
-export const API_KEY = '<36836755-9f43607b903fa703cdff42e50>';
+export const API_KEY = '36836755-9f43607b903fa703cdff42e50';
 
 export const DEFAULT_PIXABAY_PARAMS = {
   key: API_KEY,
